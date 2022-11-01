5fbdf5f25eb79804dc8d9e3e7d70498159b44177
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-11-01 01:00:00 -0300

    consulta 1 recorrido por nivel

--- Estrategia.cs
+++ Estrategia.cs
@@ -1,36 +1,63 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using tp1;
 using tp2;
 
 namespace tpfinal
 {
 
-	class Estrategia
-	{
-		public String Consulta1(ArbolBinario<DecisionData> arbol)
-		{
-			string resutl = "Implementar";
-			return resutl;
-		}
+    class Estrategia
+    {
+        public String Consulta1(ArbolBinario<DecisionData> arbol)
+        {
+            Queue<ArbolBinario<DecisionData>> Cola = new Queue<ArbolBinario<DecisionData>>();
+            ArbolBinario<DecisionData> aux;
+            Cola.Enqueue(arbol);
+            while (Cola.Count > 0)
+            {
+                aux = Cola.Dequeue();
+                aux.getDatoRaiz().ToString();
+                if (aux.getHijoDerecho() != null)
+                {
+                    Cola.Enqueue(aux.getHijoDerecho());
+                }
+                if (aux.getHijoIzquierdo() != null)
+                {
+                    Cola.Enqueue(aux.getHijoIzquierdo());
+                }
+
+            }
+            return "Fin";
 
 
-		public String Consulta2(ArbolBinario<DecisionData> arbol)
-		{
-			string resutl = "Implementar";
-			return resutl;
-		}
+        }
 
-		public String Consulta3(ArbolBinario<DecisionData> arbol)
-		{
-			string result = "Implementar";
-			return result;
-		}
 
-		public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
-		{
-			return null;
-		}
-	}
+        public String Consulta2(ArbolBinario<DecisionData> arbol)
+        {
+            string resutl = "Implementar";
+            return resutl;
+        }
+
+        public String Consulta3(ArbolBinario<DecisionData> arbol)
+        {
+            string result = "Implementar";
+            return result;
+        }
+
+        public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
+        {
+            if (clasificador.crearHoja())
+            {
+                return new ArbolBinario<DecisionData>(clasificador.obtenerPregunta());
+            }
+            else
+            {
+
+            }
+
+        }
+    }
 }
