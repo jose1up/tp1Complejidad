ce1ce10ee921a88d812fc7b7cbb022f89db2af86
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-10-31 23:04:30 -0300

    first commit

---
+++ Estrategia.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using tp1;
+using tp2;
+
+namespace tpfinal
+{
+
+	class Estrategia
+	{
+		public String Consulta1(ArbolBinario<DecisionData> arbol)
+		{
+			string resutl = "Implementar";
+			return resutl;
+		}
+
+
+		public String Consulta2(ArbolBinario<DecisionData> arbol)
+		{
+			string resutl = "Implementar";
+			return resutl;
+		}
+
+		public String Consulta3(ArbolBinario<DecisionData> arbol)
+		{
+			string result = "Implementar";
+			return result;
+		}
+
+		public ArbolBinario<DecisionData> CrearArbol(Clasificador clasificador)
+		{
+			return null;
+		}
+	}
+}
